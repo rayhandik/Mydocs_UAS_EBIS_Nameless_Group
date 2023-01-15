b8a9d8483a3d4f8decebd25b985f5968bd2a2566
Rayhan Dika Annuarjari <rayhandika4@gmail.com>
2023-01-15 20:29:16 +0700

    Add files via upload

---
+++ config/fortify.php
@@ -0,0 +1,60 @@
+<?php
+
+use App\Providers\RouteServiceProvider;
+use Laravel\Fortify\Features;
+
+return [
+
+
+
+
+    'guard' => 'web',
+
+
+
+    'passwords' => 'users',
+
+
+
+    'username' => 'email',
+
+    'email' => 'email',
+
+
+
+    'home' => RouteServiceProvider::HOME,
+
+
+    'prefix' => '',
+
+    'domain' => null,
+
+
+
+    'middleware' => ['web'],
+
+
+
+    'limiters' => [
+        'login' => 'login',
+        'two-factor' => 'two-factor',
+    ],
+
+
+
+
+    'views' => true,
+
+
+
+    'features' => [
+        Features::registration(),
+        Features::resetPasswords(),
+        Features::updateProfileInformation(),
+        Features::updatePasswords(),
+        Features::twoFactorAuthentication([
+            'confirmPassword' => true,
+        ]),
+    ],
+
+];
---
+++ config/jetstream.php
@@ -0,0 +1,29 @@
+<?php
+
+use Laravel\Jetstream\Features;
+
+return [
+
+
+    'stack' => 'livewire',
+
+
+
+    'middleware' => ['web'],
+
+
+
+    'guard' => 'sanctum',
+
+
+
+    'features' => [
+
+        Features::accountDeletion(),
+    ],
+
+
+
+    'profile_photo_disk' => 'public',
+
+];
